830daad4ab8ce369b742b1ad195b8de4996f870a
excv8426 <598867208@qq.com>
2016-08-15 07:40:30 +0800

    CvANN_MLP number

--- src/Test.java
+++ src/Test.java
@@ -2,12 +2,14 @@ import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 
+import number.NumberUtils;
+
 public class Test {
 	public static void main( String[] args ){
 		System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
 	    Mat mat = Mat.eye( 3, 3, CvType.CV_8UC1 );
 	    System.out.println( "mat = " + mat.dump() );
-	    //NumberUtils.numberstrain();
-	    //NumberUtils.numberstest();
+	    NumberUtils.numberstrain();
+	    NumberUtils.numberstest();
 	}
 }
