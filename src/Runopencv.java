8da222a7e8719a24b47808bc295d917c75d9c6ee
Yuki <Yuki@Yuki-PC>
2016-12-25 21:08:35 +0800

    DigitalMark

--- src/Runopencv.java
+++ src/Runopencv.java
@@ -7,7 +7,6 @@ public class Runopencv {
 	public static void main( String[] args ){
 		System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
 		//DigitalMark.mark("D:\\opencvdata\\input\\39.jpg","D:\\opencvdata\\input\\mark.jpg","D:\\opencvdata\\output\\390marked.jpg");
-		//DigitalMark.colourdMark("D:\\opencvdata\\input\\39.jpg","D:\\opencvdata\\input\\mark.jpg","D:\\opencvdata\\output\\390marked.jpg");
 		//DigitalMark.readMark("D:\\opencvdata\\output\\390marked1.jpg","D:\\opencvdata\\input\\39.jpg","D:\\opencvdata\\output\\readmark.jpg");
 		
 	}
