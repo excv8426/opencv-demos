fc9c47d95a45a0fe230327784ec096059aaccaa9
excv8426 <598867208@qq.com>
2016-12-25 21:24:21 +0800

    DigitalMark

--- src/Runopencv.java
+++ src/Runopencv.java
@@ -6,6 +6,7 @@ import number.RecognizeNumber;
 public class Runopencv {
 	public static void main( String[] args ){
 		System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+		
 		DigitalMark.mark("D:\\opencvdata\\input\\39.jpg","D:\\opencvdata\\input\\mark.jpg","D:\\opencvdata\\output\\390marked.jpg");
 		DigitalMark.readMark("D:\\opencvdata\\output\\390marked1.jpg","D:\\opencvdata\\input\\39.jpg","D:\\opencvdata\\output\\readmark.jpg");
 		
